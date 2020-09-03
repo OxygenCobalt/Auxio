ce687cd515658751d1709f6f94678f51246328a0
OxygenCobalt <alexyx@tutanota.com>
2020-09-03 07:49:26 -0600

    Change selection ripple to accent color
    Change the selection ripple so that it matches the chosen accent color.

--- app/src/main/java/org/oxycblt/auxio/MainActivity.kt
+++ app/src/main/java/org/oxycblt/auxio/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         Log.d(this::class.simpleName, "Activity Created.")
