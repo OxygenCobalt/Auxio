344e566aa30f998c2e99b5e890070bd18b599dc9
OxygenCobalt <alexyx@tutanota.com>
2020-11-01 15:56:53 -0700

    Update resources
    Improve resource structure/naming.

--- app/src/main/java/org/oxycblt/auxio/playback/NotificationUtils.kt
+++ app/src/main/java/org/oxycblt/auxio/playback/NotificationUtils.kt
@@ -8,7 +8,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.media.app.NotificationCompat.MediaStyle
 import org.oxycblt.auxio.MainActivity
