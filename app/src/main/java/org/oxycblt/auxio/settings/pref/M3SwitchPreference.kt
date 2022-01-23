c98d1313160f112d5645c4b82d2bea98a906a77b
OxygenCobalt <oxycblt@abstractr.net>
2022-01-23 10:53:39 -0700

    style: implement m3 switches
    Implement a facimile of the new Material 3/You switches in the settings
    menu. There's no defined spec for this yet, so I just shamelessly ripped
    the implementation from Doodle.

---
+++ app/src/main/java/org/oxycblt/auxio/settings/pref/M3SwitchPreference.kt
@@ -0,0 +1,40 @@
+package org.oxycblt.auxio.settings.pref
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import androidx.appcompat.widget.SwitchCompat
+import androidx.preference.PreferenceViewHolder
+import androidx.preference.SwitchPreferenceCompat
+import org.oxycblt.auxio.R
+import org.oxycblt.auxio.util.resolveDrawable
+import org.oxycblt.auxio.util.resolveStateList
+
+class M3SwitchPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.switchPreferenceCompatStyle,
+    defStyleRes: Int = 0
+) : SwitchPreferenceCompat(context, attrs, defStyleAttr, defStyleRes) {
+    // Lollipop cannot into ColorStateList, disable this feature on that version
+    private var needToUpdateSwitch = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+
+        if (needToUpdateSwitch) {
+            val switch = holder.findViewById(androidx.preference.R.id.switchWidget)
+
+            if (switch is SwitchCompat) {
+                switch.apply {
+                    trackDrawable = R.drawable.ui_m3_switch_track.resolveDrawable(context)
+                    trackTintList = R.color.sel_m3_switch_track.resolveStateList(context)
+                    thumbDrawable = R.drawable.ui_m3_switch_thumb.resolveDrawable(context)
+                    thumbTintList = R.color.sel_m3_switch_thumb.resolveStateList(context)
+                }
+
+                needToUpdateSwitch = false
+            }
+        }
+    }
+}
