c42da2240cf9e707641d25fa4de06a90f40fa857
Alexander Capehart <alex@oxycblt.org>
2024-02-28 23:08:09 -0700

    playback: fix add to queue again

--- app/src/main/java/org/oxycblt/auxio/playback/system/BetterShuffleOrder.kt
+++ app/src/main/java/org/oxycblt/auxio/playback/system/BetterShuffleOrder.kt
@@ -68,6 +68,9 @@ class BetterShuffleOrder(private val shuffled: IntArray) : ShuffleOrder {
             return BetterShuffleOrder(insertionCount, -1)
         }
 
+        // TODO: Fix this scuffed hacky logic
+        // TODO: Play next ordering needs to persist in unshuffle
+
         val newShuffled = IntArray(shuffled.size + insertionCount)
         val pivot: Int =
             if (insertionIndex < shuffled.size) {
@@ -87,8 +90,14 @@ class BetterShuffleOrder(private val shuffled: IntArray) : ShuffleOrder {
                 newShuffled[i + insertionCount] = currentIndex
             }
         }
-        for (i in 0 until insertionCount) {
-            newShuffled[pivot + i + 1] = insertionIndex + i + 1
+        if (insertionIndex < shuffled.size) {
+            for (i in 0 until insertionCount) {
+                newShuffled[pivot + i + 1] = insertionIndex + i + 1
+            }
+        } else {
+            for (i in 0 until insertionCount) {
+                newShuffled[pivot + i] = insertionIndex + i
+            }
         }
         return BetterShuffleOrder(newShuffled)
     }
