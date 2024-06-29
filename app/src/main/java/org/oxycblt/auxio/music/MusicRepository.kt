c8fa3892675cd1fc6c43c2933abae07a5e322b73
Alexander Capehart <alex@oxycblt.org>
2024-06-28 20:23:10 -0600

    music: add stack trace to async load task error

--- app/src/main/java/org/oxycblt/auxio/music/MusicRepository.kt
+++ app/src/main/java/org/oxycblt/auxio/music/MusicRepository.kt
@@ -438,7 +438,7 @@ constructor(
                     // to cascade to and cancel all other routines before finally bubbling up
                     // to the main extractor loop.
                     logE("MediaStore extraction failed: $e")
-                    incompleteSongs.close(Exception("MediaStore extraction failed: $e"))
+                    incompleteSongs.close(Exception("MediaStore extraction failed: ${e.stackTraceToString()}"))
                     return@async
                 }
                 incompleteSongs.close()
@@ -453,7 +453,7 @@ constructor(
                     tagExtractor.consume(incompleteSongs, completeSongs)
                 } catch (e: Exception) {
                     logE("Tag extraction failed: $e")
-                    completeSongs.close(Exception("Tag extraction failed: $e"))
+                    completeSongs.close(Exception("Tag extraction failed: ${e.stackTraceToString()}"))
                     return@async
                 }
                 completeSongs.close()
@@ -470,7 +470,7 @@ constructor(
                             completeSongs, processedSongs, separators, nameFactory)
                     } catch (e: Exception) {
                         logE("DeviceLibrary creation failed: $e")
-                        processedSongs.close(Exception("DeviceLibrary creation failed: $e"))
+                        processedSongs.close(Exception("DeviceLibrary creation failed: ${e.stackTraceToString()}"))
                         return@async Result.failure(e)
                     }
                 processedSongs.close()
