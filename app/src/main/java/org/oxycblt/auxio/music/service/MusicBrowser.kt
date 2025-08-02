52246c551057e54aacfad27004a9afa5341a3191
Alexander Capehart <alex@oxycblt.org>
2025-08-02 11:08:42 -0600

    music: correctly provide album parent context

--- app/src/main/java/org/oxycblt/auxio/music/service/MusicBrowser.kt
+++ app/src/main/java/org/oxycblt/auxio/music/service/MusicBrowser.kt
@@ -220,7 +220,7 @@ private constructor(
                 is DetailSection.Discs ->
                     section.discs.flatMap { (disc, songs) ->
                         val discString = disc.resolve(context)
-                        songs.map { it.toMediaItem(context, header(discString)) }
+                        songs.map { it.toMediaItem(context, header(discString), child(detail.parent)) }
                     }
                 else -> error("Unknown section type: $section")
             }
